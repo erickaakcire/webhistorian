7ef623e150500cff1e72eefe35b100ae746f4b8c
Ericka Menchen-Trevino <emtrevino@gmail.com>
2015-05-18 09:58:14 +0200

    First release
    Fully functional visualizations. Missing the planned submit button for
    research participation.

---
+++ background.js
@@ -0,0 +1,24 @@
+// JavaScript source code
+
+//When the user clicks on the browser action icon.
+chrome.browserAction.onClicked.addListener(function (tab) {
+    openOrFocusOptionsPage();
+});
+
+//Look to see if the extension page is open already and if not, open it.
+function openOrFocusOptionsPage() {
+    var optionsUrl = chrome.extension.getURL('index.html');
+    chrome.tabs.query({}, function (extensionTabs) {
+        var found = false;
+        for (var i = 0; i < extensionTabs.length; i++) {
+            if (optionsUrl == extensionTabs[i].url) {
+                found = true;
+                console.log("tab id: " + extensionTabs[i].id);
+                chrome.tabs.update(extensionTabs[i].id, { "selected": true });
+            }
+        }
+        if (found == false) {
+          chrome.tabs.create({ url: "index.html" });
+        }
+    });
+}
