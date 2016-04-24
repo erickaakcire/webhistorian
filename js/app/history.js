7aed6cc6cf86ef8edaab9c257d1fdd770439bf3e
Ericka Menchen-Trevino <menchent@american.edu>
2016-04-23 23:27:58 -0400

    cleaning up and preparing for the function

--- js/app/history.js
+++ js/app/history.js
@@ -573,126 +573,6 @@ define(["spin", "moment", "../app/config"], function (Spinner, moment, config)
     }
 
 
-    function timeLineViz(dataset) {
-        // based on Mike Bostock's example: http://bl.ocks.org/mbostock/1667367
-
-        d3.select("#title").append("h1").text("Time Line: Volume of web history").attr("id", "viz_title");
-
-        var margin = {top: 10, right: 10, bottom: 100, left: 40},
-            margin2 = {top: 430, right: 10, bottom: 20, left: 40},
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom,
-            height2 = 500 - margin2.top - margin2.bottom;
-
-        var parseDate = d3.time.format("%Y/%m/%d:%H").parse;
-
-        var x = d3.time.scale().range([0, width]),
-            x2 = d3.time.scale().range([0, width]),
-            y = d3.scale.linear().range([height, 0]),
-            y2 = d3.scale.linear().range([height2, 0]);
-
-        var xAxis = d3.svg.axis().scale(x).orient("bottom"),
-            xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
-            yAxis = d3.svg.axis().scale(y).orient("left");
-
-        var brush = d3.svg.brush()
-            .x(x2)
-            .on("brush", brushed);
-
-        var area = d3.svg.area()
-            .interpolate("monotone")
-            .x(function (d) {
-                return x(d.day);
-            })
-            .y0(height)
-            .y1(function (d) {
-                return y(d.count);
-            });
-
-        var area2 = d3.svg.area()
-            .interpolate("monotone")
-            .x(function (d) {
-                return x2(d.day);
-            })
-            .y0(height2)
-            .y1(function (d) {
-                return y2(d.count);
-            });
-
-        var svg = d3.select("#" + divName).append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .attr("id", "visualization");
-
-        svg.append("defs").append("clipPath")
-            .attr("id", "clip")
-            .append("rect")
-            .attr("width", width)
-            .attr("height", height);
-
-        var focus = svg.append("g")
-            .attr("class", "focus")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        var context = svg.append("g")
-            .attr("class", "context")
-            .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
-
-        for (var i = 0; i < dataset.length; i++) {
-            data = dataset[i];
-            type(data);
-            x.domain(d3.extent(data.map(data.counter)));
-            y.domain([0, d3.max(data.map(data.count))]);
-            x2.domain(x.domain());
-            y2.domain(y.domain());
-
-            focus.append("path")
-                .datum(data)
-                .attr("class", "area")
-                .attr("d", area);
-
-            focus.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis);
-
-            focus.append("g")
-                .attr("class", "y axis")
-                .call(yAxis);
-
-            context.append("path")
-                .datum(data)
-                .attr("class", "area")
-                .attr("d", area2);
-
-            context.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height2 + ")")
-                .call(xAxis2);
-
-            context.append("g")
-                .attr("class", "x brush")
-                .call(brush)
-                .selectAll("rect")
-                .attr("y", -6)
-                .attr("height", height2 + 7);
-        }
-
-        function brushed() {
-            x.domain(brush.empty() ? x2.domain() : brush.extent());
-            focus.select(".area").attr("d", area);
-            focus.select(".x.axis").call(xAxis);
-        }
-
-        function type(d) {
-            d.counter = parseDate(d.counter);
-            d.count = +d.count;
-            return d;
-        }
-
-        rmLoad();
-    }
-
     function noViz(data) {
         //nothing
         rmLoad();
@@ -862,11 +742,9 @@ define(["spin", "moment", "../app/config"], function (Spinner, moment, config)
         //Get all data into fullData1
         getUrls(noTransform, noViz, function()
         {
-            //New default, append images for visualization chooser
+            //append images for visualization chooser
             $('#viz_selector').show();
             $("#navbar").show();
-
-//	        console.log('DATA: ' + JSON.stringify(history.fullData, 2));
 
             $("#load_data").click(function () 
             { //used for researcher edition
@@ -889,6 +767,9 @@ define(["spin", "moment", "../app/config"], function (Spinner, moment, config)
                     }
                 });
             });
+            
+            $("#wir").html(         	
+            	"<h3>Week in review</h3><p>This week (day-day) you browsed the web []% (more/less) than last week (day-day). <p>Top 3 new domains in your browsing history this week:<ol><li>Domain.com [] visits</li>" );
             
             $('#upload_modal').on('show.bs.modal', function (e) 
             {
