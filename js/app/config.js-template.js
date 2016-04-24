6bc71dbaf879a674fc39ec69f7d9c9447e34f90d
Chris Karr <github@audacious-software.com>
2016-04-23 21:20:31 -0500

    Adding support for remote user site

--- js/app/config.js-template.js
+++ js/app/config.js-template.js
@@ -4,6 +4,7 @@ define([], function ()
     
     config.actionsUrl = "ACTIONS_URL_HERE";
     config.uploadUrl = "UPLOAD_URL_HERE";
+    config.reviewUrl = "REVIEW_URL_PREFIX_HERE";
 
     return config;
 });
