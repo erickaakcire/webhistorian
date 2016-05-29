962a17b87a307e67839c2b57a7f152c281241d75
Ericka Menchen-Trevino <menchent@american.edu>
2016-05-29 13:18:54 -0400

    still no transition

--- js/app/websites-visited.js
+++ js/app/websites-visited.js
@@ -76,8 +76,7 @@ define(["../app/utils", "moment"], function(utils, moment) {
         var biggestDomain = "";
         var domains = utils.countPropDomains(data, "domain");
         var domainDay2 = [];
-     	//domains object has unique domains, not using the count for this
-        
+     	//domains object has a list of unique domains, replacing the raw count with:
         for (var i = 0; i < domains.length; i++) {
             var domainDay1 = [];
             var currentDomain = domains[i].domain;
@@ -120,7 +119,7 @@ define(["../app/utils", "moment"], function(utils, moment) {
 	       var utcEnd = Date.UTC(endDate.getFullYear(), endDate.getMonth(), endDate.getDate());
 	       var diffDays = Math.ceil((utcEnd - utcStart) / oneDay);
            
-           d3.select("#title").append("h2").text("You visited " + biggestDomain + " the most for " + biggestSize + " days out of " + diffDays + ". (" + Math.round(biggestSize / diffDays * 100) + "%)");
+           $("#title").append("<h2 style=\"display:none\">You visited " + biggestDomain + " the most for " + biggestSize + " days out of " + diffDays + ". (" + Math.round(biggestSize / diffDays * 100) + "%)</h2>");
    			
         return callback(catObj, domainDay2);         
     };
@@ -139,6 +138,97 @@ define(["../app/utils", "moment"], function(utils, moment) {
         }).done(function() {
     		callback(cats);
   		});
+	}
+	
+	function changeBubble(dataset, habits) {
+		$("#visual_div").height($("#visual_div").width());
+        var r = $("#visual_div").height(),
+            format = d3.format(",d"),
+            fill = d3.scale.category20();
+
+        var bubble = d3.layout.pack()
+            .sort(null)
+            .size([r, r])
+            .padding(1.5);
+            
+        var siteClasses = utils.classes(dataset);//dataset
+            
+        var vis = d3.select("#visual_div").append("svg")
+            .attr("width", r)
+            .attr("height", r)
+            .attr("class", "bubble")
+            .attr("id", "visualization");
+
+        var tooltip = d3.select("body")
+		    .append("div")
+		    .style("position", "absolute")
+		    .style("z-index", "10")
+		    .style("visibility", "hidden")
+		    .style("color", "white")
+		    .style("padding", "8px")
+		    .style("background-color", "rgba(0, 0, 0, 0.75)")
+		    .style("border-radius", "6px")
+		    .style("font", "12px sans-serif")
+		    .text("tooltip");
+        
+        var node = vis.selectAll("g.node")
+            .data(bubble.nodes(siteClasses)//from dataset
+                .filter(function (d) {
+                    return !d.children;
+                }))
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+
+        node.append("circle")
+            .attr("r", function (d) {
+                return d.r;
+            })
+            .style("fill", function (d) {
+                return fill(d.packageName);
+            })
+            .on("mouseover", function(d) {
+	              if (habits===0){
+	              	tooltip.text(d.className + ", Visits: " + format(d.value) + ", Category: " + d.packageName);
+	              }
+	              else if (habits===1){
+	              	tooltip.text(d.className + ", Days: " + format(d.value) + ", Category: " + d.packageName);
+	              }
+	              
+	              tooltip.style("visibility", "visible");
+	          })
+		      
+		      .on("mousemove", function() {
+		          return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+		      })
+		      .on("mouseout", function(){
+		      	return tooltip.style("visibility", "hidden");
+		      	})
+            ;
+
+        node.append("text")
+            .attr("text-anchor", "middle")
+            .attr("dy", ".3em")
+            .text(function (d) {
+            return d.className.substring(0, d.r / 3);
+        })
+        	.on("mouseover", function(d) {
+        		if(habits===0){
+        			tooltip.text(d.className + ", Visits: " + format(d.value) + ", Category: " + d.packageName);
+        		}
+        		else if (habits===1){
+        			tooltip.text(d.className + ", Days: " + format(d.value) + ", Category: " + d.packageName);
+        		}
+	              
+	              tooltip.style("visibility", "visible");
+		      })
+		     .on("mousemove", function() {
+		          return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+		      })
+		     .on("mouseout", function(){return tooltip.style("visibility", "hidden");})
+		     ;
 	}
     
     visualization.display = function(history, data)
@@ -166,18 +256,20 @@ define(["../app/utils", "moment"], function(utils, moment) {
 
 			var numDomains = utils.countUniqueProperty(data, "domain");
 			
+			var datasetV = visualization.catData(filteredData, categories, getVisitData);
+			var datasetH = visualization.catData(filteredData, categories, getHabitData);
+			
 			if (habits === 0) {
-				var dataset = visualization.catData(filteredData, categories, getVisitData);
 				d3.select("#title").append("h1").text("What websites do you visit most?").attr("id", "viz_title");
 				d3.select("#title").append("h2").text(numDomains + " websites visited from " + moment(startDate).format("MMM D, YYYY") + " to: " + moment(endDate).format("MMM D, YYYY")).attr("id", "viz_subtitle");
 	       		$("#above_visual").html("<div class=\"btn-group\" data-toggle=\"buttons\"> <label class=\"btn btn-primary active\"> <input type=\"radio\" name=\"options\" id=\"visits\" autocomplete=\"off\" checked> All Visits  </label> <label class=\"btn btn-primary\"> <input type=\"radio\"name=\"options\" id=\"habits\" autocomplete=\"off\"> Daily Habits  </label></div>");
-				
-	        	} else if (habits === 1) {
-				var dataset = visualization.catData(filteredData, categories, getHabitData);
+				changeBubble(datasetV, habits);
+	        	} 
+	        	else if (habits === 1) {
 				$("#title").prepend("<h1>What websites do you visit regularly</h1>");
-				//d3.select("#title").prepend("h1").text("What websites do you visit regularly?").attr("id", "viz_title");
-				//d3.select("#title").append("h2").text("You visited " + "biggestDomain" + " the most for " + "biggestSize" + " days out of " + diffDays + ". (" "+ Math.round(biggestSize / diffDays * 100)" + "%)");
 				$("#above_visual").html("<div class=\"btn-group\" data-toggle=\"buttons\"> <label class=\"btn btn-primary\"> <input type=\"radio\" name=\"options\" id=\"visits\" autocomplete=\"off\"> All Visits  </label> <label class=\"btn btn-primary active\"> <input type=\"radio\"name=\"options\" id=\"habits\" autocomplete=\"off\" checked> Daily Habits  </label></div>");
+				$("#title h2").css("display","block");
+				changeBubble(datasetH, habits);
 			}
 
 	        
@@ -195,94 +287,7 @@ define(["../app/utils", "moment"], function(utils, moment) {
 		    });
     
 
-			$("#visual_div").height($("#visual_div").width());
-	        var r = $("#visual_div").height(),
-	            format = d3.format(",d"),
-	            fill = d3.scale.category20();
-	
-	        var bubble = d3.layout.pack()
-	            .sort(null)
-	            .size([r, r])
-	            .padding(1.5);
-	            
-	        var siteClasses = utils.classes(dataset);//dataset
-	            
-	        var vis = d3.select("#visual_div").append("svg")
-	            .attr("width", r)
-	            .attr("height", r)
-	            .attr("class", "bubble")
-	            .attr("id", "visualization");
-	
-	        var tooltip = d3.select("body")
-			    .append("div")
-			    .style("position", "absolute")
-			    .style("z-index", "10")
-			    .style("visibility", "hidden")
-			    .style("color", "white")
-			    .style("padding", "8px")
-			    .style("background-color", "rgba(0, 0, 0, 0.75)")
-			    .style("border-radius", "6px")
-			    .style("font", "12px sans-serif")
-			    .text("tooltip");
-	        
-	        var node = vis.selectAll("g.node")
-	            .data(bubble.nodes(siteClasses)//from dataset
-	                .filter(function (d) {
-	                    return !d.children;
-	                }))
-	            .enter().append("g")
-	            .attr("class", "node")
-	            .attr("transform", function (d) {
-	                return "translate(" + d.x + "," + d.y + ")";
-	            });
-	
-	        node.append("circle")
-	            .attr("r", function (d) {
-	                return d.r;
-	            })
-	            .style("fill", function (d) {
-	                return fill(d.packageName);
-	            })
-	            .on("mouseover", function(d) {
-		              if (habits===0){
-		              	tooltip.text(d.className + ", Visits: " + format(d.value) + ", Category: " + d.packageName);
-		              }
-		              else if (habits===1){
-		              	tooltip.text(d.className + ", Days: " + format(d.value) + ", Category: " + d.packageName);
-		              }
-		              
-		              tooltip.style("visibility", "visible");
-		          })
-			      
-			      .on("mousemove", function() {
-			          return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
-			      })
-			      .on("mouseout", function(){
-			      	return tooltip.style("visibility", "hidden");
-			      	})
-	            ;
-	
-	        node.append("text")
-	            .attr("text-anchor", "middle")
-	            .attr("dy", ".3em")
-	            .text(function (d) {
-	            return d.className.substring(0, d.r / 3);
-            })
-            	.on("mouseover", function(d) {
-            		if(habits===0){
-            			tooltip.text(d.className + ", Visits: " + format(d.value) + ", Category: " + d.packageName);
-            		}
-            		else if (habits===1){
-            			tooltip.text(d.className + ", Days: " + format(d.value) + ", Category: " + d.packageName);
-            		}
-		              
-		              tooltip.style("visibility", "visible");
-			      })
-			     .on("mousemove", function() {
-			          return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
-			      })
-			     .on("mouseout", function(){return tooltip.style("visibility", "hidden");})
-			     ;
+
 		});
     };
     
